e0205621ec341d9450d3e8d2801c93577c9c25bd
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-10-05 17:33:05 +0530

    Add files via upload

---
+++ EX_comments_&_Punds.py
@@ -0,0 +1,7 @@
+#   Comments & Pounds Character
+# in this file I'am explaining "#" this Character
+# they also said that "#" is also used as hide or Disable the program temprorary
+print ("I could have code like this.") # and after "#" everything is ignored.
+# you can also use a comment to "disable" or comment out code:
+# print ("This won't run.")
+print("This will run.")
---
+++ String_and_Text.py
@@ -0,0 +1,20 @@
+types_of_people = 10# this is veriable
+x = f"There are {types_of_people} types of people."#This is also a veriable but here we are adding or embeding veriable in string with help of f format
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."#Same as above but two veriable is added.
+
+print(x)#In this line we are printing veriable
+print(y)
+
+print(f"I said: {x}")#In this, we are printing string & veriable value by useing "f" format method.
+print(f"I also said: '{y}'")
+hilarious = False#Its boolean type data type, here we are giving False boolean value to character.
+joke_evaluation = "Isn't that joke so funny?!  {}" # Its veriable contain string only
+
+w = "This is the left side of . . ."
+e = "a string with a right side."
+
+print(w + e) # here we are adding both string and printing it symulteniously.
+# Abobve veriable 'w' and 'e' are both string, so because of that using '+' plus operator we can join them.
