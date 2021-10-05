0458491a3a382698055e39e1d020f4f55a7133ec
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-10-05 17:05:13 +0530

    Delete ex1.1.py

--- ex1.1.py
+++
@@ -1,7 +0,0 @@
-print ("in this file I tryed experimenting on name like 'ex1.1.py' this, let's see wil this work or not.?")
-print ("well it worked perfectly")
-print ("Its nice to see my thought or thinking is not far from the mark's.")
-# now i'm typing just using "#" symbol, because aouthor said. but I already know that this hole line will consider as Comment!!!!!  so, whatever you type code, symbol or any other character used in python will become 'comment' )
-print ("Now after runing this file, I realized that comments are used to mention code meaning or /n help coder to read the 'code'")
-print ("this is just experiment",
-\n"to see that can I able print in new line.")
