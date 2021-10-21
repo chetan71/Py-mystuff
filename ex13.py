e759b4f544251e5f1b993b95a517bda3a8430845
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-10-21 08:25:25 +0530

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+#read the WYSS sectonfor how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+"""When I'm trying to run this file I didn't get 7 & 8 line print. How can I solve this?
+Search Online!"""
