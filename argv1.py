c92bb34dad312661758094f21907db0749e0b846
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-10-22 18:05:28 +0530

    b
    office

--- argv1.py
+++ argv1.py
@@ -1,17 +1,8 @@
 from sys import argv
 # read the WYSS section for how to run this
-<<<<<<< HEAD
 King_of_fruit, My_education, Currently_addmission_in, Featuer_plan = argv
 
 print("this fruit is king of all fruit:", King_of_fruit)
 print("I completed my:", My_education)
 print("I'm currently studying:", Currently_addmission_in)
 print("I'm gona complet :", Featuer_plan, "this course & start working as programmer.")
-=======
-script, first, second, third = argv
-
-print("The script is called:", script)
-print("Your first variable is:", first)
-print("Your second variable is:", second)
-print("Your third variable is:", third)
->>>>>>> dc517e7cc5210ebd0f511c313f02d7482b5dbf7c
