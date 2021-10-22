110ec6616638b9443d96ac5f16bc20cefa059d24
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-10-22 07:10:53 +0530

    from home

---
+++ argv1.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+King_of_fruit, My_education, Currently_addmission_in, Featuer_plan = argv
+
+print("this fruit is king of all fruit:", King_of_fruit)
+print("I completed my:", My_education)
+print("I'm currently studying:", Currently_addmission_in)
+print("I'm gona complet :", Featuer_plan, "this course & start working as programmer.")
--- ex13.py
+++ ex13.py
@@ -8,3 +8,4 @@ print("Your second variable is:", second)
 print("Your third variable is:", third)
 """When I'm trying to run this file I didn't get 7 & 8 line print. How can I solve this?
 Search Online!"""
+# The problem is solved automatically.
