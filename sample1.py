166d16afbe03521542ad02f15aab6aecee4beb32
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-11-26 17:50:41 +0530

    Create sample1.py

---
+++ sample1.py
@@ -0,0 +1,7 @@
+print("How old are you?", end='')
+age = input()
+print("How tall are you?")
+height = input()
+print("How much do you weigh?", end='')
+weigh = input()
+print(f"so, your {age} year old, {height} tall & {weigh} heavy.")
