cd0ef3a38729d17ed08584f96f543e426354cf55
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-12-21 17:38:14 +0530

    Create ex15_copy.py

---
+++ ex15_copy.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script, filename = argv
+txt = open(ex15_sample_txt_file.py)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
