aa1f884b2aa62dd2eb17bb814cec5022ba317534
chetan71 <91386497+chetan71@users.noreply.github.com>
2021-12-22 11:00:48 +0530

    2nd day after diwali

--- ex15.py
+++ ex15.py
@@ -1,15 +1,24 @@
 from sys import argv
+#here sys is package & argv is feature from that package
+#So we are just saying get the "argv" feature from that "sys" package.
 
 script, filename = argv
+#get argv commond line arguments (when running the file) and assigning it to two veriable
 
 txt = open(filename)
+# here we are opening veriable filename and assigning it to txt veriables
 
-print(f"here's your file {fileneame}:")
+print(f"here's your file {filename}:")
+#here we are using "f" type formating to insert veriable filename and then printing it
 print(txt.read())
+#here we are using another new fucntion "read". here we are saying read txt veriable and then print it
 
 print("Type the filename again:")
 file_again = input(">")
+#here we are simply printing string to understan the user what he have to give input
+#then we asking for input and assinging it to file_again veriables
 
 txt_again = open(file_again)
+#Same as above
 
 print(txt_again.read())
---
+++ ex15_asign4.py
@@ -0,0 +1,17 @@
+"""Assignment No. 4-5
+4. Get rid of the lines 10-15 where you use input and run the script again.
+5. Use only input and try the script that way. Why is one way of getting the
+filename better than another?"""
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+txt1 = open(input("FILENAME>"))# I didn't put double quests so I got error
+print(f"this is the file name {filename}")
+
+print(txt1.read())
