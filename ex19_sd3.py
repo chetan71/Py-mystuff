1174257c16f7a8928bee389004c9c4c313fa1f94
chetan71 <bhange71@gmail.com>
2021-12-30 22:50:53 +0530

    Day3 after 2Days break

---
+++ ex19_sd3.py
@@ -0,0 +1,11 @@
+def pc_main_component (processor, ram):
+    print(f"I got {processor} and {ram} GB ram")
+    print(f"oooh! It's very good processor and with that {ram} amount of ram \n it will run like cheetha!!!")
+    print("It's best for gaming. Like PUBG, CyberPunk etc.")
+
+p = input("name of processor")
+
+pc_main_component(p, 16)
+
+
+pc_main_component(p, 12)
