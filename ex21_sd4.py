8177fe46ac75fb82da4992006bf90c7452e211b6
chetan71 <bhange71@gmail.com>
2022-01-02 23:35:02 +0530

    2.01.22

---
+++ ex21_sd4.py
@@ -0,0 +1,27 @@
+print("Here is the puzzle that book given me.")
+def divide(a, b):
+    print(f"DIVIDING: {a} / {b}")
+    return a / b
+
+def multiply(a, b):
+    print(f"MULTIPLYING: {a} * {b}")
+    return a * b
+
+def subtract(a, b):
+    print(f"SUBTRACTING: {a} - {b}")
+    return a - b
+
+def add(a, b):
+    print(f"ADDING: {a} + {b}")
+    return a + b
+
+age = divide(50, 2)
+
+Height = multiply(180, 25)
+
+Weight = subtract(74, 4500)
+
+IQ = add(35, -4426)
+
+print(f"Age: {age}, Height: {Height}, Weight: {Weight}, IQ: {IQ}")
+print("I think that's the normal way of printing puzzle.")
