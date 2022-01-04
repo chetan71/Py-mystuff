e0f50c28fbf328d325f14c4d5f24fd30c601e4cf
chetan71 <bhange71@gmail.com>
2022-01-04 23:48:01 +0530

    4

---
+++ end_sample.py
@@ -0,0 +1,12 @@
+end1 = 'c'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+
+print(end1 + end2 + end3 + end4 + end5, end='')
+
+print(end1 + end2 + end3 + end4 + end5, end=" ")
+
+#print(end1 + end2 + end3, end=" ", end4 + end5)
+#print(end1 + end2 + end3, end="", end4 + end5)
