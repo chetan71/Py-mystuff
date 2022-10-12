2c22eb997875c8d85c28a4f96c2ed8f3dd5b3b04
chetan <bhange71@gmail.com>
2022-10-12 09:18:30 +0530

    Create Stuff.py

---
+++ Stuff.py
@@ -0,0 +1,7 @@
+Stuff = “ric bread jam coffee  chips milk”
+stuff = ‘ ‘.split(stuff)
+print(stuff)
+print(stuff)
+print(len(stuff))
+print(‘Wait! there are not 10 things in this List’)
+more_stuff = [‘Biscuits’, ‘Masala’, ‘Chaha Pati’, ‘Sugar’, ‘Maggy’, ‘Sauce’]
